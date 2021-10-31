1fde152fa68c538eeb4568c35bb567dc3fc2799a
zhouhanseng <z308114274@gmail.com>
2021-11-01 03:32:43 +0800

    chore: list page coming soon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,17 @@
 import * as React from "react"
 import Logo from '../images/Logo.svg'
+import TwitterIcon from "../images/twitter.svg";
 
-export function Footer() {
+export function Footer(props) {
   return (
-    <footer className="max-w-6xl mx-auto mb-16 w-9/12 max-sm:w-10/12">
-      <div className="w-full border-blue-underline border-b">
-        <img className="w-32 mb-3" alt="" src={Logo} />
+    <footer className={(props.isContentEmpty ? "absolute bottom-0 left-1/2 transform -translate-x-2/4" : "") + " max-w-6xl mx-auto mb-16 w-9/12 max-sm:w-10/12 max-sm:mb-8"}>
+      <div className="w-full border-blue-underline border-b flex justify-between items-center md:h-auto h-16">
+        <img className="w-32 md:mb-3 mb-1 max-sm:w-24" alt="" src={Logo} />
+        <div className="md:hidden flex w-60 max-sm:w-36">
+          <div className="w-32 h-full absolute top-0 cursor-pointer z-2" />
+          <div className="w-32 h-full absolute left-32 top-0 cursor-pointer z-2" />
+          <img src={TwitterIcon} alt="" className="w-full" />
+        </div>        
       </div>
       <div className="w-full mt-1 h-8 flex justify-between items-center text-white max-sm:text-xxs">
         <p>Privacy · Imprint</p>
